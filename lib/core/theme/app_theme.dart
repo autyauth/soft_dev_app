637093d6c4324b434ab98761bd207ec2191d132b
autyauth <thanakornrattanapornchai@gmail.com>
2023-09-18 00:07:15 +0700

    edit theme

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:soft_dev_app/theme/pallete.dart';
+import 'package:soft_dev_app/core/theme/pallete.dart';
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
