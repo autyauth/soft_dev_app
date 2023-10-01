2712b3ca4d6868c0aef0ad11a8db4eef5ce1550a
Nattaphon <nattaphonnuayen@gmail.com>
2023-10-01 16:53:32 +0700

    d

--- lib/features/workout/screens/widget/widget_tree.dart
+++ lib/features/workout/screens/widget/widget_tree.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soft_dev_app/core/theme/pallete.dart';
 import 'package:soft_dev_app/features/workout/screens/page/profile_page.dart';
 import 'package:soft_dev_app/features/workout/screens/page/select_excercise_page.dart';
 
@@ -18,12 +19,42 @@ class _WidgetTreeState extends State<WidgetTree> {
     return Scaffold(
       body: pages[currentPage],
       bottomNavigationBar: NavigationBar(
-        destinations:  [
-          NavigationDestination(icon: SizedBox(height: 25,child: Image.asset("assets/icons/dumble.png"),) , label: 'Workout'),
-          NavigationDestination(icon: SizedBox(height: 25,child: Image.asset("assets/icons/shop.png"),), label: 'Shop'),
-          NavigationDestination(icon: SizedBox(height: 25,child: Image.asset("assets/icons/home.png"),), label: 'Home'),
-          NavigationDestination(icon: SizedBox(height: 25,child: Image.asset("assets/icons/medal.png"),), label: 'Leader board'),
-          NavigationDestination(icon: SizedBox(height: 25,child: Image.asset("assets/icons/person.png"),), label: 'Profile'),
+        destinations: [
+          NavigationDestination(
+              icon: SizedBox(
+                height: 25,
+                child: Image.asset("assets/icons/dumble.png"),
+              ),
+              selectedIcon: Container(
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Palette.whiteColor,),
+              ),
+              label: 'Workout'),
+          NavigationDestination(
+              icon: SizedBox(
+                height: 25,
+                child: Image.asset("assets/icons/shop.png"),
+              ),
+              label: 'Shop'),
+          NavigationDestination(
+              icon: SizedBox(
+                height: 25,
+                child: Image.asset("assets/icons/home.png"),
+              ),
+              label: 'Home'),
+          NavigationDestination(
+              icon: SizedBox(
+                height: 25,
+                child: Image.asset("assets/icons/medal.png"),
+              ),
+              label: 'Leader board'),
+          NavigationDestination(
+              icon: SizedBox(
+                height: 25,
+                child: Image.asset("assets/icons/person.png"),
+              ),
+              label: 'Profile'),
         ],
         selectedIndex: currentPage,
         onDestinationSelected: (int value) {
@@ -31,6 +62,7 @@ class _WidgetTreeState extends State<WidgetTree> {
             currentPage = value;
           });
         },
+        backgroundColor: Palette.greyColor,
       ),
     );
   }
