724d9a6820f6728e10004eec3c1131d436488d15
Nattaphon <nattaphonnuayen@gmail.com>
2023-10-04 12:23:17 +0700

    editbox

--- lib/features/workout/screens/page/edit_profile_page.dart
+++ lib/features/workout/screens/page/edit_profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soft_dev_app/core/theme/pallete.dart';
 import 'package:soft_dev_app/features/workout/screens/widget/back.dart';
 
 class EditProfilePage extends StatelessWidget {
@@ -6,27 +7,42 @@ class EditProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/profile_background.png'),
-            fit: BoxFit.cover,
+        body: Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/profile_background.png'),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
-        width: double.infinity,
-        child: const Column(
-          children: [
-            SizedBox(
-              height: 40,
-            ),
-            SizedBox(
-              height: 85,
-              child: ImgBackButton(),
-            )
-          ],
+        Positioned(
+          top: screenHeight * 0.05,
+          left: screenWidth * 0.03,
+          child: ImgBackButton(),
         ),
-      ),
-    );
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+            margin: EdgeInsets.only(
+                left: screenWidth * 0.1,
+                right: screenWidth * 0.1,
+                top: screenHeight * 0.15,
+                bottom: screenHeight * 0.25),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Palette.orangeColor, 
+                width: 2.0, 
+              ),
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
