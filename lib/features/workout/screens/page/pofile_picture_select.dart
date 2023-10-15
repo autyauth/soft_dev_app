55471944f6b857e0a70b0ad7c359f4dc8be5a97e
Nattaphon <nattaphonnuayen@gmail.com>
2023-10-15 10:32:18 +0700

    temp for image picker

--- lib/features/workout/screens/page/pofile_picture_select.dart
+++ lib/features/workout/screens/page/pofile_picture_select.dart
@@ -1,16 +1,65 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:soft_dev_app/core/theme/theme.dart';
 import 'package:soft_dev_app/features/workout/screens/modal/Userprofile.dart';
 import 'package:soft_dev_app/features/workout/screens/widget/back_btn.dart';
+import 'package:soft_dev_app/features/workout/screens/widget/gradient_button.dart';
 import 'package:soft_dev_app/features/workout/screens/widget/outline_text.dart';
 
-class ProfilePictureSelect extends StatelessWidget {
+class ProfilePictureSelect extends StatefulWidget {
+  @override
+  State<ProfilePictureSelect> createState() => _ProfilePictureSelectState();
+}
 
+class _ProfilePictureSelectState extends State<ProfilePictureSelect> {
+  final buttonwidth = 300.0;
+
+  File ? _pickedImage;
+
+  Future<UserProfile?> fetchUserProfileData() async {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> userDoc = await FirebaseFirestore
+          .instance
+          .collection('userProfile')
+          .doc('/cXZMVx6sIaGJnK7FgSIO')
+          .get();
+
+      if (userDoc.exists) {
+        Map<String, dynamic> userData = userDoc.data()!;
+        return UserProfile.fromMap(userData);
+      } else {
+        print('User document does not exist.');
+        return null;
+      }
+    } catch (e) {
+      print('Error fetching user profile data: $e');
+      return null;
+    }
+  }
+  UserProfile? userProfile;
+  UserProfile? updatedUserProfile;
+
+  void initState() {
+    super.initState();
+    fetchUserProfileData().then((profile) {
+      if (profile != null) {
+        setState(() {
+          userProfile = profile;
+
+          imageURL = userProfile?.imageUrl ?? "";
+        });
+      }
+    });
+  }
+  String imageURL = '';
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
+  print(imageURL);
     return Scaffold(
       body: Stack(
         children: [
@@ -25,7 +74,7 @@ class ProfilePictureSelect extends StatelessWidget {
           Positioned(
             top: screenHeight * 0.04,
             left: screenWidth * 0.1,
-            child:  ImgBackButton(),
+            child: ImgBackButton(),
           ),
           Positioned(
             top: screenHeight * 0.04,
@@ -40,8 +89,58 @@ class ProfilePictureSelect extends StatelessWidget {
                 outlineColor: Colors.black,
                 outlineWidth: 2.0),
           ),
+          Positioned(
+            child: Container(
+              margin: EdgeInsets.only(
+                  left: screenWidth * 0.085,
+                  top: screenHeight * 0.15,),
+              height: screenHeight * 0.6,
+              width: 0.83*screenWidth,
+              decoration: BoxDecoration(color: Colors.white),child: _pickedImage != null ? Image.file(_pickedImage!): Text('pleaseSelect Image'),
+            ),
+          ),
+          Positioned(
+            top: screenHeight * 0.85,
+            left: screenWidth * 0.5 - buttonwidth / 2,
+            child: Column(
+              children: [
+                GradientButton(
+                  height: 44,
+                  width: buttonwidth,
+                  colorsArray: const [
+                    Palette.orangeCreamColor2,
+                    Palette.orangeColor,
+                    Palette.orangeColor
+                  ],
+                  buttonText: 'Select Image',
+                  onPress: () {
+                   _pickImageFromGallery();
+                  },
+                ),
+                SizedBox(height: 10),
+                GradientButton(
+                  height: 44,
+                  width: buttonwidth,
+                  colorsArray: const [
+                    Palette.orangeCreamColor2,
+                    Palette.orangeColor,
+                    Palette.orangeColor
+                  ],
+                  buttonText: 'Save',
+                  onPress: () {},
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
+  }
+  Future _pickImageFromGallery() async{
+    final pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _pickedImage = File(pickedImage!.path); 
+    });
   }
 }
