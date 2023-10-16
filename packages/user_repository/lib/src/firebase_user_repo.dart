244b7cc68a9c4fe4f7083cec24e95a6d6461aed1
AD <buaseefah@gmail.com>
2023-10-16 15:35:47 +0700

    fix database collections

--- packages/user_repository/lib/src/firebase_user_repo.dart
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -7,7 +7,7 @@ import 'package:user_repository/src/user_repo.dart';
 
 class FirebaseUserRepo implements UserRepository {
   final FirebaseAuth _firebaseAuth;
-  final usersCollection = FirebaseFirestore.instance.collection('users');
+  final usersCollection = FirebaseFirestore.instance.collection('usersProfile');
 
   FirebaseUserRepo({
     FirebaseAuth? firebaseAuth,
